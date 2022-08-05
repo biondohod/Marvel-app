98e1513c0bcd13052b56619c8648662a58cf9344
Andrey <biondohod@gmail.com>
2022-08-05 12:57:44 +0200

    Добавляет верстку шапки и блока со случайным персонажем

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AppHeader from '../appHeader/AppHeader';
+import RandomChar from '../randomChar/RandomChar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <AppHeader/>
+        <main>
+          <RandomChar/>
+        </main>
+      </div>
+    )
+  }
 }
 
 export default App;
