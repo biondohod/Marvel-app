4af9866817d2d224450eed0210ad5b2c45e342ed
Andrey <biondohod@gmail.com>
2022-08-10 18:28:25 +0200

    Переписывает все компоненты в функциональные(кроме ErrorBoundary и MarvelService)

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,32 +1,28 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { Component } from 'react';
 
 import './appHeader.scss';
 
-class AppHeader extends Component {
-    
-    render() {
-        return( 
-            <header className='app__header'>
-                <h1 className="app__title">
-                    <a href="#">
-                    <span>Marvel</span> information portal
-                    </a>
-                </h1>
-                <nav className="app__menu">
-                    <ul className='app__menu__list'>
-                        <li className='app__menu__item'>
-                            <a href="#">Characters</a>
-                        </li>
-                        /
-                        <li className='app__menu__item'>
-                            <a href="#">Comics</a>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-        );
-    }
+const AppHeader = () => {
+    return( 
+        <header className='app__header'>
+            <h1 className="app__title">
+                <a href="#">
+                <span>Marvel</span> information portal
+                </a>
+            </h1>
+            <nav className="app__menu">
+                <ul className='app__menu__list'>
+                    <li className='app__menu__item'>
+                        <a href="#">Characters</a>
+                    </li>
+                    /
+                    <li className='app__menu__item'>
+                        <a href="#">Comics</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default AppHeader;
